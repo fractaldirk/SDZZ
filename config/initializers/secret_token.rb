bef66f72af04bf1a536b4ad5a308c6959a678d2c
fractaldirk <dirkgrandjean@live.nl>
2012-07-13 23:03:16 +1200

    facebook sign in

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SDZZ::Application.config.secret_token = '6b42687966ab13fe319ddb09f7d5ec581d323ea8fd48b9976d7516977a9de888920058d531efd86fdb3896513ef741edd5574c889dc7a466b0a0d4feeedb7d33'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SDZZ::Application.config.session_store :cookie_store, key: '_SDZZ_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SDZZ::Application.config.session_store :active_record_store
